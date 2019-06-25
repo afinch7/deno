36c244d9d05a115066b46de27a3a1b2c3aeefda9
afinch7 <andyfinch7@gmail.com>
2019-06-25 11:39:00 -0400

    format test plugin

--- tests/plugin/lib.rs
+++ tests/plugin/lib.rs
@@ -1,7 +1,7 @@
 // Copyright 2018-2019 the Deno authors. All rights reserved. MIT license.
-use deno::{PinnedBuf, Buf};
-use deno::plugins::{PluginOpResult, new_plugin_error};
+use deno::plugins::{new_plugin_error, PluginOpResult};
 use deno::Op;
+use deno::{Buf, PinnedBuf};
 
 #[macro_use]
 extern crate deno;
@@ -11,17 +11,20 @@ pub fn op_test_op(
   data: &[u8],
   zero_copy: Option<PinnedBuf>,
 ) -> PluginOpResult {
-    if !is_sync {
-        return Err(new_plugin_error(String::from("Async not supported!")));
-    }
-    if let Some(buf) = zero_copy {
-        let data_str = std::str::from_utf8(&data[..]).unwrap();
-        let buf_str = std::str::from_utf8(&buf[..]).unwrap();
-        println!("Hello from native bindings. data: {} | zero_copy: {}", data_str, buf_str);
-    }
-    let result = b"test";
-    let result_box: Buf = Box::new(*result);
-    Ok(Op::Sync(result_box))
+  if !is_sync {
+    return Err(new_plugin_error(String::from("Async not supported!")));
+  }
+  if let Some(buf) = zero_copy {
+    let data_str = std::str::from_utf8(&data[..]).unwrap();
+    let buf_str = std::str::from_utf8(&buf[..]).unwrap();
+    println!(
+      "Hello from native bindings. data: {} | zero_copy: {}",
+      data_str, buf_str
+    );
+  }
+  let result = b"test";
+  let result_box: Buf = Box::new(*result);
+  Ok(Op::Sync(result_box))
 }
 
 declare_plugin_op!(test_op, op_test_op);
